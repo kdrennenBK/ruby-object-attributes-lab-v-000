7ed6ff39eff8ea3ec20a73898bb687b7944ca150
Kate Drennen <k8ted11@gmail.com>
2018-01-14 21:46:14 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,23 +1,24 @@
 class Person
-  def name(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
+  attr_accessor :name, :job
+  # def name(name)
+  #   @name = name
+  # end
+  #
+  # def name
+  #   @name
+  # end
 
   def name=(new_name)
     @name = new_name
   end
 
-  def job(job)
-    @job = job
-  end
-
-  def job
-    @job
-  end
+  # def job(job)
+  #   @job = job
+  # end
+  #
+  # def job
+  #   @job
+  # end
 
   def job=(new_job)
     @job = new_job
