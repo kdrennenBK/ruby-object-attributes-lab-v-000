dab8723da4b5d2ce08ac49032d1190caf0e6fa45
Kate Drennen <k8ted11@gmail.com>
2018-01-14 20:53:01 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,12 @@
 class Dog
   attr_accessor :dog, :breed
-  ?
+  # def name(name)
+  #   @name = name
+  # end
+  #
+  # def name
+  #   @name
+  # end
 
   def name=(new_name)
     @name = new_name
