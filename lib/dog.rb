df9a852519066d505b718f55e23b4b4328bec05a
Kate Drennen <k8ted11@gmail.com>
2018-01-14 20:56:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,15 +8,17 @@ class Dog
     @name
   end
 
-  def name=(new_name)
-    @name = new_name
-  end
+
 
   def breed=(breed)
     @breed = dog_breed
   end
-  #
-  # def breed
-  #   @breed
-  # end
+
+  def breed
+    @breed
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
 end
